9543ca3c8eaa6a53d140a1eae39c8d9c7fb514c5
TopHatHipster <43381730+TopHatHipster@users.noreply.github.com>
2019-08-11 17:04:40 +0200

    Update config.py
    Converted user input strings into raw strings so doubling '\' isn't necessary anymore as Python will read it as intended to.

--- config.py
+++ config.py
@@ -1,18 +1,18 @@
-# List of owned games, 1 per line. Rom name must match lookup. All '\' in path must be doubled.
-# Defaults to inside the plugin directory unless changed. To change, put the path inside the "", for example: game_list = "G:\\My Games\\NDS\\NDS-list.txt"
-game_list = ""
+# List of owned games, 1 per line. Rom name must match lookup.
+# Defaults to inside the plugin directory unless changed. To change, put the path inside the "", for example: game_list = r"G:\My Games\NDS\NDS-list.txt"
+game_list = r""
 
-# .DAT file for lookup. All '\' in path must be doubled.
-# Defaults to inside the plugin directory unless changed. To change, put the path inside the "", for example: game_dat = "G:\\My Games\\NDS\\NDS.dat"
-game_dat = ""
+# .DAT file for lookup.
+# Defaults to inside the plugin directory unless changed. To change, put the path inside the "", for example: game_dat = r"G:\My Games\NDS\NDS.dat"
+game_dat = r""
 
-# Location of 'installed' local games. All '\' in path must be doubled, and it must end with '\\'
-# As an example, it could look like this: roms_path = "G:\\My Games\\NDS\\"
-roms_path = "REQUIRED_FOR_LOCAL_GAMES"
+# Location of 'installed' local games. It must end with '\'
+# As an example, it could look like this: roms_path = r"G:\My Games\NDS\"
+roms_path = r"REQUIRED_FOR_LOCAL_GAMES"
 
-# Path to emulator executable. All '\' in path must be doubled
-# As an example, it could look like this: emu_path = "G:\\My Emulator\\myemulator.exe"
-emu_path = "REQUIRED_FOR_LOCAL_GAMES"
+# Path to emulator executable.
+# As an example, it could look like this: emu_path = r"G:\My Emulator\myemulator.exe"
+emu_path = r"REQUIRED_FOR_LOCAL_GAMES"
 
 # Pick a username
-username = "Username"
+username = r"Username"
